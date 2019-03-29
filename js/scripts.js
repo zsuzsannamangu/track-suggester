cff6a54aa9815e935829acaf3b3c43f63f4a8ce7
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-03-29 13:24:28 -0700

    modify javascript code to fix branching problems - still not working

--- js/scripts.js
+++ js/scripts.js
@@ -22,22 +22,24 @@ $(document).ready(function() {
 
   $("button").submit(function(event) {
     var largeorsmall = $("#largeorsmall").val();
-    var tasks1 = $("#tasks1").val();
-    var tasks2 = $("#tasks2").val();
-    var tasks3 = $("#tasks3").val();
+    var tasksone = $("#tasksone").val();
+    var taskstwo = $("#taskstwo").val();
     var firstnameInput = $("input#firstname").val();
-      if (largeorsmall === "large" && tasks1 === "csharp") {
-        $("#suggestion").show();
-        $("#csharp").show();
-      } else if (largeorsmall === "large" && tasks1 === "java1" && tasks2 === "php1") {
-        $("#suggestion").show();
-        $("#java").show();
-      } else if (largeorsmall === "large" && tasks2 === "php1" && tasks3 === "php2") {
-        $("#suggestion").show();
-        $("#php").show();
-      } else if (largeorsmall === "small" && tasks3 === "ruby") {
+      if (largeorsmall === "small" && tasksone === "csharp" || "javaone" && taskstwo === "javatwo" || "phpone" || "phptwo") {
         $("#suggestion").show();
         $("#ruby").show();
+      } else if (largeorsmall === "large" && tasksone === "csharp" && taskstwo === "javatwo" || "phpone" || "phptwo") {
+        $("#suggestion").show();
+        $("#csharp").show();
+      } else if (largeorsmall === "large" && tasksone === "javaone" && taskstwo === "javatwo") {
+        $("#suggestion").show();
+        $("#java").show();
+      } else if (largeorsmall === "large" && tasksone === "csharp" || "javaone" && taskstwo === "phpone" || "phptwo") {
+        $("#suggestion").show();
+        $("#php").show();
+      } else if (largeorsmall === "large" && tasksone === "csharp" || "javaone" && taskstwo === "javatwo" || "phpone" || "phptwo") {
+        $("#suggestion").show();
+        $("#php").show();
       } else {
         console.log("error");
       }
