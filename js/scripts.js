d3b5043228a32948a475fae527d74ef530a0a1fe
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-03-29 11:59:49 -0700

    keep working on javascript branching, try fixing unknown errors

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
         $("#design").show();
       } else if (frontorback === "back") {
         $(".nextquestions").show();
+        $(".btntwo").show();
         $(".answer").hide();
       } else {
         console.log("error");
@@ -15,23 +16,30 @@ $(document).ready(function() {
   event.preventDefault();
   });
 
-  $("button").click(function(event) {
+  $("form").submit(function(event) {
     var largeorsmall = $("#largeorsmall").val();
-      if (largeorsmall === "small") {
-        $(".tasks").hide();
+    var tasks1 = $("#tasks1").val();
+    var tasks2 = $("#tasks2").val();
+    var tasks3 = $("#tasks3").val();
+      if (largeorsmall === "large" && tasks1 === "csharp") {
+        $("#suggestion").show();
+        $("#csharp").show();
+      } else if (largeorsmall === "large" && tasks1 === "java1" && tasks2 === "php1") {
+        $("#suggestion").show();
+        $("#java").show();
+      } else if (largeorsmall === "large" && tasks2 === "php1" && tasks3 === "php2") {
+        $("#suggestion").show();
+        $("#php").show();
+      } else if (largeorsmall === "small" && tasks3 === "ruby") {
         $("#suggestion").show();
         $("#ruby").show();
-      } else if (largeorsmall === "large") {
-        $(".tasks").show();
-        $(".answer").hide();
+
+
+
+
       } else {
         console.log("error");
       }
   event.preventDefault();
   });
 });
-//
-//
-// var tasks1 = $("#tasks1").val();
-// var tasks2 = $("#tasks2").val();
-// var tasks3 = $("#tasks3").val();
