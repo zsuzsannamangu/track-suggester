afe2587af7af882fc2c41cd0a95dd8b74aeaab08
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-03-29 15:14:47 -0700

    perform debugging techniques, fix issue with second button.

--- js/scripts.js
+++ js/scripts.js
@@ -22,56 +22,42 @@ $(document).ready(function() {
   event.preventDefault();
   });
 
-  $("#button2").submit(function(event) {
+  $("#button2").click(function(event) {
     var largeorsmall = $("#largeorsmall").val();
-    // var tasksone = $("#tasksone").val();
-    // var taskstwo = $("#taskstwo").val();
+    var tasksone = $("#tasksone").val();
+    var taskstwo = $("#taskstwo").val();
     var firstnameInput = $("input#firstname").val();
 
     $(".firstname").text(firstnameInput);
 
-      if (largeorsmall === "small") {
+      if (largeorsmall === "small" && tasksone === ("csharp" || "javaone") && taskstwo === ("javatwo" || "phpone" || "phptwo")) {
         $("#suggestion").show();
+        $(".answer").show();
         $("#ruby").show();
         $("#endmessage").show();
-      } else if (largeorsmall === "large") {
+      } else if (largeorsmall === "large" && tasksone === "csharp" && taskstwo === ("javatwo" || "phpone" || "phptwo")) {
         $("#suggestion").show();
+        $(".answer").show();
         $("#csharp").show();
+        $("#endmessage").show();
+      } else if (largeorsmall === "large" && tasksone === "javaone" && taskstwo === "javatwo") {
+        $("#suggestion").show();
+        $(".answer").show();
+        $("#java").show();
+        $("#endmessage").show();
+      } else if (largeorsmall === "large" && tasksone === ("csharp" || "javaone") && taskstwo === ("phpone" || "phptwo")) {
+        $("#suggestion").show();
+        $(".answer").show();
+        $("#php").show();
+        $("#endmessage").show();
+      } else if (largeorsmall === "large" && tasksone === ("csharp" || "javaone") && taskstwo === ("javatwo" || "phpone" || "phptwo")) {
+        $("#suggestion").show();
+        $(".answer").show();
+        $("#php").show();
         $("#endmessage").show();
       } else {
         console.log("error");
       }
-
-      // if (largeorsmall === "small" && tasksone === "csharp" || "javaone" && taskstwo === "javatwo" || "phpone" || "phptwo") {
-      //   $("#suggestion").show();
-      //   $(".answer").show();
-      //   $("#ruby").show();
-      //   $("#endmessage").show();
-      // } else if (largeorsmall === "large" && tasksone === "csharp" && taskstwo === "javatwo" || "phpone" || "phptwo") {
-      //   $("#suggestion").show();
-      //   $(".answer").show();
-      //   $("#csharp").show();
-      //   $("#endmessage").show();
-      // } else if (largeorsmall === "large" && tasksone === "javaone" && taskstwo === "javatwo") {
-      //   $("#suggestion").show();
-      //   $(".answer").show();
-      //   $("#java").show();
-      //   $("#endmessage").show();
-      // } else if (largeorsmall === "large" && tasksone === "csharp" || "javaone" && taskstwo === "phpone" || "phptwo") {
-      //   $("#suggestion").show();
-      //   $(".answer").show();
-      //   $("#php").show();
-      //   $("#endmessage").show();
-      // } else if (largeorsmall === "large" && tasksone === "csharp" || "javaone" && taskstwo === "javatwo" || "phpone" || "phptwo") {
-      //   $("#suggestion").show();
-      //   $(".answer").show();
-      //   $("#php").show();
-      //   $("#endmessage").show();
-      // } else {
-      //   console.log("error");
-      // }
-      // $(".answer").show();
-      $(".answer").show();
 
     event.preventDefault();
   });
