160062c6492ab3185584bffdd792067e4ceb87be
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-03-29 11:27:16 -0700

    add javascript branching - not working so far

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
   $("button").click(function(event) {
     var frontorback = $("#frontorback").val();
-    var largeorsmall = $("#largeorsmall").val();
-    var tasks1 = $("#tasks1").val();
-    var tasks2 = $("#tasks2").val();
-    var tasks3 = $("#tasks3").val();
       if (frontorback === "front") {
-        $(".nextquestions:not(#frontorback)").hide();
-        $(".answer:not(#design)").hide();
+        $(".backend").hide();
+        $(".tasks").hide();
+        $("#suggestion").show();
         $("#design").show();
       } else if (frontorback === "back") {
         $(".nextquestions").show();
@@ -15,10 +12,26 @@ $(document).ready(function() {
       } else {
         console.log("error");
       }
-      
-        $(".nextquestions").show();
-        $(".answer").show();
   event.preventDefault();
+  });
 
+  $("button").click(function(event) {
+    var largeorsmall = $("#largeorsmall").val();
+      if (largeorsmall === "small") {
+        $(".tasks").hide();
+        $("#suggestion").show();
+        $("#ruby").show();
+      } else if (largeorsmall === "large") {
+        $(".tasks").show();
+        $(".answer").hide();
+      } else {
+        console.log("error");
+      }
+  event.preventDefault();
   });
 });
+//
+//
+// var tasks1 = $("#tasks1").val();
+// var tasks2 = $("#tasks2").val();
+// var tasks3 = $("#tasks3").val();
