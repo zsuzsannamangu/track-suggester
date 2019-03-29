f9b247e7c95434b9800e58f074d5cc4c33539cf6
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-03-29 10:24:36 -0700

    write some javascript branching code - so far not working what I wrote

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
+  $("button").click(function(event) {
+    var frontorback = $("#frontorback").val();
+    var largeorsmall = $("#largeorsmall").val();
+    var tasks1 = $("#tasks1").val();
+    var tasks2 = $("#tasks2").val();
+    var tasks3 = $("#tasks3").val();
+      if (frontorback === "front") {
+        $(".nextquestions:not(#frontorback)").hide();
+        $(".answer:not(#design)").hide();
+        $("#design").show();
+      } else if (frontorback === "back") {
+        $(".nextquestions").show();
+        $(".answer").hide();
+      } else {
+        console.log("error");
+      }
+      
+        $(".nextquestions").show();
+        $(".answer").show();
+  event.preventDefault();
 
-
+  });
 });
-
-
-var frontorback = $("#frontorback").val();
-var largeorsmall = $("#largeorsmall").val();
-var tasks = $("#tasks").val();
